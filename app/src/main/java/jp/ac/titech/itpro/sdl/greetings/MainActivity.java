85119e2cdd490ad1eea928b702d4aec0927b54a1
marina <yamasawa@bi.c.titech.ac.jp>
2017-04-21 16:31:32 +0900

    add TAG

--- app/src/main/java/jp/ac/titech/itpro/sdl/greetings/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/greetings/MainActivity.java
@@ -6,16 +6,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.view.View;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView outputView;
     private EditText inputName;
     private Button okButton;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
 
         outputView = (TextView)findViewById(R.id.output_view);
@@ -33,4 +36,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         "\nNice to see yoU!");
         break; }
         }
+    @Override
+    protected void onStart() { // 同様にして onResume, onRestart, onPause, onStop,
+        super.onStart(); // onDestroy も実装すること.全て protected で,返値は
+        Log.d(TAG, "onStart"); // void,引数はなしである.
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+
+
 }
